f3f2ead986ac45f197b4e6d054166f4b55d22fcc
Aisulu <aisulutulepova4@gmail.com>
2024-10-30 22:55:58 +0500

    home work3

---
+++ variable.js
@@ -0,0 +1,14 @@
+let name = "Аты:Али";
+console.log("Аты:Али:" + typeof name );
+
+let age = 25
+console.log("Жасы:" + age + typeof age);
+
+let isStudent = true;
+let hasLicense = false;
+console.log("Студент пе?:" +true + typeof true);
+
+let age1 = "25";
+console.log("Жасы:"+ 25+ typeof age1)
+
+
